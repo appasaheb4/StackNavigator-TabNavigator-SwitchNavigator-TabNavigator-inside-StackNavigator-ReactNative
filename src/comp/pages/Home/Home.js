732d8ddc61e7a9d3a66ad6d2e3c7239c0d1c8cb9
appasaheb4 <appasaheb.itworks@gmail.com>
2018-10-05 09:32:56 +0530

    Project Create.

---
+++ src/comp/pages/Home/Home.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Picker,
+  Image,
+  AsyncStorage
+} from "react-native";
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <View>  
+        <Text>Home</Text>
+      </View>
+    );
+  }
+}
